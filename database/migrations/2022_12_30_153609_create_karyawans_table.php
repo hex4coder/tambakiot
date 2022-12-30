1de88be5a534311ffc7a68832792435208894130
hex4coder <the.programmer.luyo@gmail.com>
2022-12-30 23:39:58 +0800

    Penambahan model dan controller untuk karyawan

---
+++ database/migrations/2022_12_30_153609_create_karyawans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('karyawans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->string('nama');
+            $table->ipAddress('ipAddress')->default('192.168.4.1');
+            $table->string('ssid');
+            $table->string('password');
+            $table->string('kode')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('karyawans');
+    }
+};
