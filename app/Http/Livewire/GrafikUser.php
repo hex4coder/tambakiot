ffdee386b381ae9057b644ef566f287684c87489
hex4coder <the.programmer.luyo@gmail.com>
2023-01-15 12:35:05 +0000

    Perbaikan grafik dashboard

--- app/Http/Livewire/GrafikUser.php
+++ app/Http/Livewire/GrafikUser.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire;
 
 use App\Models\Karyawan;
 use App\Models\Panen;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
@@ -12,9 +13,24 @@ class GrafikUser extends Component
     public function render()
     {
         $jumlahKaryawanSaya = Karyawan::where('user_id', Auth::id())->count();
-        $jumlahPanen = Panen::with('karyawan', function($query) {
+        $allPanen = Panen::with(['karyawan' => function($query) {
             $query->where('user_id', Auth::id());
-        })->count();
+        }])->get();
+
+        $jumlahPanen = 0;
+        foreach ($allPanen as $k => $v) {
+            $updated_at = $v['updated_at'];
+            $arr = explode('-', $updated_at);
+            $bulan = (int)$arr[1];
+            $tahun = (int)$arr[0];
+            $tahunSekarang = Carbon::now()->year;
+            $bulanSekarang = Carbon::now()->month;
+            
+            if(($tahun == $tahunSekarang) && ($bulan == $bulanSekarang)) {
+                $jumlahPanen++;
+            }
+        }
+
         $data = [
             'jumlah_panen' => $jumlahPanen,
             'jumlah_karyawan' => $jumlahKaryawanSaya,
