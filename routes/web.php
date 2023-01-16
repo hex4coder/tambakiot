e90b440eea6f5bb3799f617d04c94847ae48569b
hex4coder <the.programmer.luyo@gmail.com>
2023-01-16 09:37:26 +0000

    Perbaikan hapus foto udang

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\KaryawanController;
 use App\Http\Controllers\PanenController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\URL;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,13 +18,10 @@ use Illuminate\Support\Facades\Storage;
 */
 
 Route::get('/', function () {
-    // echo public_path();
-    // echo "\r\n";
-    // echo '<br>';
-    // echo Storage::url('/var/www/html/storage/app/public/foto/ZUgEt1Xct4l6cn5B4ypxlcnm9da0ILxcjGD59zZr.jpg');
-    // echo '<br>';
-    // echo public_path('foto') . '/CJKeKAEzwf4wi4T9ChGuAW6nwFFG6dJTaISmoYTD.png';
-    return view('welcome');
+    
+    echo storage_path('app/public') ;
+    
+    //return view('welcome');
 });
 
 Route::get('/karyawan', [KaryawanController::class, 'index'])->name('karyawan');
